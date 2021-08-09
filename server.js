cf668aa52c654b7ede2ff5af34fb35ab785d8d2d
omarsharifgr <omarsharif.os@yahoo.com>
2021-08-10 00:49:42 +0100

    Update

--- server.js
+++ server.js
@@ -1,26 +1,38 @@
 const express = require("express");
 const app = express();
+
 const cors = require("cors");
 app.use(cors());
 app.use(express.json())
 app.use(express.static(__dirname))
 var bodyParser = require("body-parser")
 app.use(bodyParser.json())
+
 const { Pool } = require("pg");
-const exampleResponse = require("./exampleresponse.json")
-const port = process.env.PORT || 5000;
 
 const pool = new Pool({
-    // connectionString: process.env.DATABASE_URL,
-    user: 'jpnihcxooyrini',
-    host: 'ec2-63-34-97-163.eu-west-1.compute.amazonaws.com',
-    database: 'd8c3grvol7tv0n',
-    password: "2ff0a67b775bec4ba47e6bbbad6d7ab96e2fc6807f2e0194a5fad2616c7ae306",
-    port: 5432
-});
+    user: "bspovtylvxhavh",
+    host: "ec2-63-34-97-163.eu-west-1.compute.amazonaws.com",
+    database: "dem18udcue6vjn",
+    password: "4003a3d0545e4e16a1051f078b53be22fe0c955cbecf2e26d6c475814ec37b0b",
+    port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
+
+
 app.get("/hotels", function (req, res) {
   pool.query("SELECT * FROM *")
     .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
+const poolVariable = "CREATE TABLE customers (email VARCHAR(120) NOT NULL)"
+app.put("/hotel", function (req, res) {
+  pool
+    .query(poolVariable)
+    .then((result) => res.json("result"))
     .catch((e) => console.error(e));
 });
 app.listen(port, () => console.log(`Listening on port ${port}`));
